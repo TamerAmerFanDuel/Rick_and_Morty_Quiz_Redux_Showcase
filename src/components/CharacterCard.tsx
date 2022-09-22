46584a23cd20a98462a61cce6c6028c88d4da8fe
Tamer <tamer.amer@fanduel.com>
2022-09-22 13:38:07 +0100

    added PageSelector and completed pagination

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,11 +1,22 @@
 import { Grid, Card, CardMedia, CardContent, TextField } from "@mui/material"
 import { CharacterResult } from "../store/types"
+import { useState } from "react"
 
-const CharacterCard = (character: CharacterResult) => {
+interface CharacterCardProps {
+    character: CharacterResult
+}
+
+const CharacterCard: React.FC<CharacterCardProps> = ({
+    character
+}) => {
+
+
+
+    const [ name, setName ] = useState("")
 
     return(
-        <Grid key={character.id} item xs={3}>
-            <Card sx={{ maxWidth: 345 }}>
+        <Grid item xs={3}>
+            <Card>
                 <CardMedia 
                     id={String(character.id)}
                     component="img"
@@ -14,7 +25,8 @@ const CharacterCard = (character: CharacterResult) => {
                     alt={character.name}
                 />
                 <CardContent>
-                    <TextField label="Who am I?.." variant="outlined" />                    
+
+                    <TextField label="Who am I?.." variant="outlined" value={name} onChange={event => {setName(event.target.value)}}/>                    
                 </CardContent>
             </Card>
         </Grid>
--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -1,23 +1,21 @@
 import { StoreState, useAppDispatch, useAppSelector } from "../store/store"
 import { useEffect } from "react"
 import { fetchCharacters } from "../store/characterSlice"
-import { Grid, Pagination } from "@mui/material"
+import { Box, Grid, Pagination } from "@mui/material"
 import CharacterCard from "./CharacterCard"
+import { Container } from "@mui/system"
+import PageSelector from "./PageSelector"
 
 const CharacterList = () => {
-
+    
     const fetchedCharacters = useAppSelector((state:StoreState)=> state.character)
     const dispatch = useAppDispatch()
-
+    
     useEffect(()=>{
         dispatch(fetchCharacters(1))
     },[dispatch])
-
-    const showResults = fetchedCharacters.results.map((character)=>{
-        return(
-            CharacterCard(character)
-        )
-    })
+    
+    
 
     const handleChange=(pageChange: number)=>{
         dispatch(fetchCharacters(pageChange))
@@ -25,12 +23,24 @@ const CharacterList = () => {
 
 
     return(
-        <>
-            <Pagination count={fetchedCharacters.info.pages} page={fetchedCharacters.info.currentPage} onChange={(_,pageChange)=>{handleChange(pageChange)}}/>
+        <Container maxWidth="lg">
+            <Box sx={{ flexGrow: 3 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <PageSelector/>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Pagination count={fetchedCharacters.info.pages} page={fetchedCharacters.info.currentPage} onChange={(_,pageChange)=>{handleChange(pageChange)}}/>
+                    </Grid>
+                </Grid>
+            </Box>
             <Grid container spacing={1}>
-                {showResults}
+            {fetchedCharacters.results.map((character)=>
+                    <CharacterCard key={character.id} character={character}/>
+                )
+            }
             </Grid>
-        </>
+        </Container>
     )
 }
 
