9b55401ce0317943c21f0214bc92b4ad68de2ad4
TamerAmerFanDuel <112479173+TamerAmerFanDuel@users.noreply.github.com>
2022-10-05 11:50:32 +0100

    BEAR-2691: added form validation (#4)
    * added form validation
    
    * refactored code and added imageless results to filter list
    
    * added ! to filter that was returning wrong data

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,36 +1,60 @@
-import { Grid, Card, CardMedia, CardContent, TextField } from "@mui/material"
+import { Grid, Card, CardMedia, CardContent, TextField, styled } from "@mui/material"
 import { CharacterResult } from "../store/types"
 import { useState } from "react"
 
 interface CharacterCardProps {
-    character: CharacterResult
+	character: CharacterResult
 }
 
+const ValidationTextField = styled(TextField)({
+	'& input:disabled + fieldset': {
+	  	borderColor: 'green !important',
+	  	borderWidth: 2,
+	},
+	"& input:disabled": {
+		"-webkit-text-fill-color": "green !important"
+	},
+	'& input:invalid + fieldset': {
+		borderColor: 'red',
+		borderWidth: 2,
+	},
+	'& input:valid:focus + fieldset': {
+		borderLeftWidth: 6,
+		padding: '4px !important',
+	},
+})
+
 const CharacterCard: React.FC<CharacterCardProps> = ({
-    character
+	character
 }) => {
+	const [ name, setName ] = useState("")
+	const isGuessCorrect = name.toLocaleLowerCase().trim() === character.name.toLocaleLowerCase().trim()
 
-
-
-    const [ name, setName ] = useState("")
-
-    return(
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Card>
-                <CardMedia 
-                    id={String(character.id)}
-                    component="img"
-                    height="300"
-                    src={character.image}
-                    alt={character.name}
-                />
-                <CardContent>
-
-                    <TextField label="Who am I?.." variant="outlined" value={name} onChange={event => {setName(event.target.value)}}/>                    
-                </CardContent>
-            </Card>
-        </Grid>
-    )
+	return(
+		<Grid item xs={12} sm={6} md={4} lg={3}>
+			<Card>
+				<CardMedia 
+					id={String(character.id)}
+					component="img"
+					height="300"
+					src={character.image}
+					alt={character.name}
+				/>
+				<CardContent>
+					<ValidationTextField
+						id="validation-outlined-input"
+						disabled={isGuessCorrect}
+						value={isGuessCorrect ? character.name : name}
+						variant="outlined"
+						label={isGuessCorrect ? <span>✅</span> : "Who am I?.."}
+						inputProps={{pattern: [character.name.toLocaleLowerCase()]}}
+						onChange={event => setName(event.target.value)}
+						required
+					/>
+				</CardContent>
+			</Card>
+		</Grid>
+	)
 }
 
 export default CharacterCard
